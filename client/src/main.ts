5c52d5e0300f553e91d1c3bb398958a8eb6933a9
andreea <andreea337@gmail.com>
2024-06-18 00:20:11 +0800

    use tailwind

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,9 +3,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import "@/assets/tailwind.css";
 import PrimeVue from "primevue/config";
 import "primevue/resources/themes/lara-light-amber/theme.css";
-import "@/assets/tailwind.css";
+
+
 const app = createApp(App)
 // 註冊
 app.use(router); 
